edb8f07ba027f155208ef4b35e74c4e43b413dfb
Radovan Prijić <rprijic3619rn@raf.rs>
2021-12-10 12:27:18 +0100

    mala izmena u glavnom appu

--- rest_api.js
+++ rest_api.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { sequelize } = require('./models');
 const app = express();
 const PORT = 5000;
-
-//const users = require('./routes/users');
-//const tournaments = require('./routes/tournaments');
-//const organisers = require('./routes/organisers');
-//const results = require('./routes/results');
-//app.use('/admin', users);
-//app.use('/admin', tournaments);
-//app.use('/admin', organisers);
-//app.use('/admin', results);
 //app.use(bodyParser.json());
+
+const users = require('./routes/users');
+const tournaments = require('./routes/tournaments');
+const organisers = require('./routes/organisers');
+const results = require('./routes/results');
+app.use('/admin', users);
+app.use('/admin', tournaments);
+app.use('/admin', organisers);
+app.use('/admin', results);
 
 //app.use(express.static(path.join(__dirname, 'static'))); 
 // Ovo je za komunikaciju sa GUI-jem posle.
 
-const { sequelize } = require('./models');
-const sequelize = new Sequelize('chess_database', 'root', null, {
+/* const sequelize = new Sequelize('chess_database', 'root', null, {
     host: '127.0.0.1',
     dialect: 'mysql'
-});
+}); */
 
 sequelize.authenticate()
     .then(() => console.log('Database connected.'))
